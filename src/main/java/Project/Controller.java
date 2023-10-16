8b7470f6e6947e6e9b3cb4db0c74362fc89d10a4
Azharddin <masterhac65@gmail.com>
2023-10-16 10:26:23 +0530

    added

---
+++ src/main/java/Project/Controller.java
@@ -0,0 +1,14 @@
+package Project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+		@GetMapping(value="/get")
+		public String hi() {
+			return "Vanthuruchu da Mavanae";
+		}
+
+	
+}
